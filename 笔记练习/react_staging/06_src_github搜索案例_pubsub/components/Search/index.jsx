a1ff21c2e0c348f1244e0e073b24b6de2ad6ecbf
bjfu_ltl <1002705465@qq.com>
2021-11-23 16:50:18 +0800

    07_fetch_demo

--- 笔记练习/react_staging/06_src_github搜索案例_pubsub/components/Search/index.jsx
+++ 笔记练习/react_staging/06_src_github搜索案例_pubsub/components/Search/index.jsx
@@ -1,16 +1,15 @@
 import React, { Component } from 'react'
-// import axios from 'axios'
+import axios from 'axios'
 import PubSub from 'pubsub-js'
 
 export default class Search extends Component {
-  search = async ()=>{
+  search = ()=>{
 		//获取用户的输入(连续解构赋值+重命名)
 		const {keyWordElement:{value:keyWord}} = this
 		//发送请求前通知List更新状态
 		PubSub.publish('bjfu',{isFirst:false,isLoading:true})
-		
-    //#region 发送网络请求 --使用axios发送
-		/*axios.get(`/api1/search/users?q=${keyWord}`).then(
+		//发送网络请求
+		axios.get(`/api1/search/users?q=${keyWord}`).then(
 			response => {
 				//请求成功后通知List更新状态
 				PubSub.publish('bjfu',{isLoading:false,users:response.data.items})
@@ -19,17 +18,7 @@ export default class Search extends Component {
 				//请求失败后通知App更新状态
 				PubSub.publish('bjfu',{isLoading:false,err:error.message})
 			}
-		)*/
-    //#endregion
-  
-    // 发送网络请求 --使用fetch发送
-      try {
-        const response = await fetch(`/api1/search/users?q=${keyWord}`)
-        const data = await response.json()
-        PubSub.publish('bjfu',{isLoading:false,users:data.items})
-      } catch (error) {
-        PubSub.publish('bjfu',{isLoading:false,err:error.message})
-      }
+		)
 	}
 
   render() {
