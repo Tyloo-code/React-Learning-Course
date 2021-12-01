f4db314a248f855850ace1c0e53a744a8eb388ca
bjfu_ltl <1002705465@qq.com>
2021-12-01 22:22:37 +0800

    03-react-router.md

--- 笔记练习/react_staging/src/App.jsx
+++ 笔记练习/react_staging/src/App.jsx
@@ -1,40 +1,19 @@
 import React, { Component } from 'react'
-import {Route,Switch,Redirect} from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Header from './components/Header'
-import MyNavLink from './components/MyNavLink'
-
+import { Button,DatePicker } from 'antd'
+import 'antd/dist/antd.css'
+import {WechatOutlined,WeiboOutlined,SearchOutlined} from '@ant-design/icons'
+const { RangePicker } = DatePicker;
 export default class App extends Component {
   render() {
     return (
       <div>
-        <div className="row">
-          <div className="col-xs-offset-2 col-xs-8">
-           <Header/>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-xs-2 col-xs-offset-2">
-            <div className="list-group">
-             {/*  <a className="list-group-item" href="./about.html">About</a>
-                  <a className="list-group-item active" href="./home.html">Home</a> */}
-                <MyNavLink to="/about">About</MyNavLink>
-                <MyNavLink to="/home">Home</MyNavLink>
-            </div>
-          </div>
-          <div className="col-xs-6">
-            <div className="panel">
-              <div className="panel-body">
-                <Switch>
-                  <Route path='/about' component={About}/>
-                  <Route path='/home' component={Home}/>
-                  <Redirect to="/about"/>
-                </Switch>
-              </div>
-            </div>
-          </div>
-        </div>
+        App...
+        <button>点我</button>
+        <Button type="primary">按钮1</Button>
+        <WechatOutlined />
+        <WeiboOutlined />
+        <DatePicker/>
+        <RangePicker/>
       </div>
     )
   }
