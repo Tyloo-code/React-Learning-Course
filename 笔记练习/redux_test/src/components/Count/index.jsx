4ba0c409b396bfbd53738f608cc2ac787b756ae6
bjfu_ltl <1002705465@qq.com>
2021-12-03 09:16:10 +0800

    2_redux_mini

--- 笔记练习/redux_test/src/components/Count/index.jsx
+++ 笔记练习/redux_test/src/components/Count/index.jsx
@@ -1,40 +1,38 @@
 import React, { Component } from 'react'
+import store from '../../../../../源码/redux_test/2_src_redux精简版/redux/store'
 
 export default class Count extends Component {
-  state={count:0}
+  state={carName:'奔驰G63'}
 
   increment = () => {
     const {value} = this.selectNumber
-    const {count} = this.state
-    this.setState({count:count+value*1})
+    store.dispatch({type:'increment',data:value*1})
   }
 
   decrement = () => {
     const {value} = this.selectNumber
-    const {count} = this.state
-    this.setState({count:count-value*1})
+    store.dispatch({type:'decrement',data:value*1})
   }
 
   incrementIfOdd = () => {
     const {value} = this.selectNumber
-    const {count} = this.state
+    const {count} = store.getState()
     if(count % 2 !== 0){
-      this.setState({count:count+value*1})
+    	store.dispatch({type:'increment',data:value*1})
     }
   }
 
   incrementAsync = () => {
     const {value} = this.selectNumber
-    const {count} = this.state
     setTimeout(() => {
-      this.setState({count:count+value*1})
+      store.dispatch({type:'increment',data:value*1})
     }, 500)
   }
 
   render() {
     return (
       <div>
-        <h1>当前求和为：{this.state.count}</h1>
+        <h1>当前求和为：{store.getState()}</h1>
         <select ref={c => this.selectNumber = c}>
           <option value="1">1</option>
           <option value="2">2</option>
