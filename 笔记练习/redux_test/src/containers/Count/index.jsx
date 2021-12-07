698b0b92606d92ad53ddc3bc01e6f8669dd7949e
bjfu_ltl <1002705465@qq.com>
2021-12-07 15:02:17 +0800

    7_src_react-redux完整版

--- 笔记练习/redux_test/src/containers/Count/index.jsx
+++ 笔记练习/redux_test/src/containers/Count/index.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 import {
-  createIncrementAction,
-  createDecrementAction,
-  createIncrementAsyncAction
-} from '../../redux/count_action'
+  increment,
+  decrement,
+  incrementAsync
+} from '../../redux/actions/count'
 import { connect } from "react-redux";
 
 class Count extends Component {
@@ -12,49 +12,49 @@ class Count extends Component {
 
   increment = () => {
     const {value} = this.selectNumber
-    this.props.jia(value*1)
+    this.props.increment(value*1)
   }
 
   decrement = () => {
     const {value} = this.selectNumber
-    this.props.jian(value*1)
+    this.props.decrement(value*1)
   }
 
   incrementIfOdd = () => {
     const {value} = this.selectNumber
     if(this.props.count % 2 !== 0){
-      this.props.jia(value*1)
+      this.props.increment(value*1)
     }
   }
 
   incrementAsync = () => {
     const {value} = this.selectNumber
-    this.props.jiaAsync(value*1,500)
+    this.props.incrementAsync(value*1,500)
   }
 
   render() {
     return (
-      <div>
-        <h1>当前求和为：{this.props.count}</h1>
-        <select ref={c => this.selectNumber = c}>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-        </select>&nbsp;
-        <button onClick={this.increment}>+</button>&nbsp;
-        <button onClick={this.decrement}>-</button>&nbsp;
-        <button onClick={this.incrementIfOdd}>当前和为奇数再加</button>&nbsp;
-        <button onClick={this.incrementAsync}>异步加</button>&nbsp;
-      </div>
-    )
+			<div>
+				<h2>我是Count组件,下方组件总人数为:{this.props.renshu}</h2>
+				<h4>当前求和为：{this.props.count}</h4>
+				<select ref={c => this.selectNumber = c}>
+					<option value="1">1</option>
+					<option value="2">2</option>
+					<option value="3">3</option>
+				</select>&nbsp;
+				<button onClick={this.increment}>+</button>&nbsp;
+				<button onClick={this.decrement}>-</button>&nbsp;
+				<button onClick={this.incrementIfOdd}>当前求和为奇数再加</button>&nbsp;
+				<button onClick={this.incrementAsync}>异步加</button>&nbsp;
+			</div>
+		)
   }
 }
 
 export default connect(
-  state => ({count:state}),
-  {
-    jia:createIncrementAction,
-		jian:createDecrementAction,
-		jiaAsync:createIncrementAsyncAction,
-  }
+  state => ({
+    count:state.count,
+		personCount:state.persons.length
+  }),
+  {increment,decrement,incrementAsync}
 )(Count)
---
+++ 笔记练习/redux_test/src/redux/reducers/index.js
@@ -0,0 +1,11 @@
+import {combineReducers} from 'redux'
+//引入为Count组件服务的reducer
+import count from './count'
+//引入为Person组件服务的reducer
+import persons from './person'
+
+//汇总所有的reducer变为一个总的reducer
+export default combineReducers({
+	count,
+	persons
+})
